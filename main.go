c001db24c71f001d2df26b9b9bb3be46e901c3b0
Abdullah Alhusaini <abdullah@daarb.dev>
2024-02-27 21:43:55 +0300

    prevent imguiini from showing  up in repository
    add input box to main page

--- main.go
+++ main.go
@@ -4,55 +4,58 @@ import (
 	imgui "github.com/gabstv/cimgui-go"
 	ebimgui "github.com/gabstv/ebiten-imgui/v3"
 	"github.com/hajimehoshi/ebiten/v2"
-	htgotts "github.com/hegedustibor/htgo-tts"
-	"github.com/hegedustibor/htgo-tts/handlers"
-	"github.com/hegedustibor/htgo-tts/voices"
 )
 
 func main() {
 	ebiten.SetWindowSize(1024, 768)
 
-	gg := &G{}
+	gg := &Game{text: ""}
 
 	ebiten.RunGame(gg)
 
 }
 
-type G struct {
-	w, h int
+type Game struct {
+	width, height int
+	text          string
 }
 
-func (g *G) Draw(screen *ebiten.Image) {
+func (g *Game) Draw(screen *ebiten.Image) {
 
 	ebimgui.Draw(screen)
 }
 
-func (g *G) Update() error {
+func (g *Game) Update() error {
 	ebimgui.Update(1.0 / 60.0)
 
 	ebimgui.BeginFrame()
 	defer ebimgui.EndFrame()
+
 	imgui.BeginV("shite", nil, imgui.WindowFlagsNoTitleBar|imgui.WindowFlagsNoMove)
 	defer imgui.End()
 
 	imgui.SetWindowPosVec2(imgui.NewVec2(0, 0))
-	imgui.SetWindowSizeVec2(imgui.NewVec2(float32(g.w), float32(g.h)))
+	imgui.SetWindowSizeVec2(imgui.NewVec2(float32(g.width), float32(g.height)))
 
-	if imgui.Button("Don't press me.. I have press phobia") {
-		speech := htgotts.Speech{Folder: "audio", Language: voices.English, Handler: &handlers.Native{}}
-		speech.Speak("Did you just press the button? When the label said not to press it. Cringe")
-		speech.Speak("hello world")
-		//	os.RemoveAll(speech.Folder)
+	if imgui.InputTextWithHint("input", "", &g.text, imgui.InputTextFlagsEnterReturnsTrue, callback) {
+		g.text = ""
 	}
+
+	imgui.Text(g.text)
 
 	return nil
 }
 
-func (g *G) Layout(outsideWidth, outsideHeight int) (int, int) {
+func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
 
-	g.w = outsideWidth
-	g.h = outsideHeight
+	g.width = outsideWidth
+	g.height = outsideHeight
 
-	ebimgui.SetDisplaySize(float32(g.w), float32(g.h))
-	return g.w, g.h
+	ebimgui.SetDisplaySize(float32(g.width), float32(g.height))
+	return g.width, g.height
+}
+
+func callback(data imgui.InputTextCallbackData) int {
+
+	return 0
 }
