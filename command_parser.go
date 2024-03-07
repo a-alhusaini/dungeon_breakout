b081d79a65140b7bd2b598cdf6217346883e8ff7
Abdullah Alhusaini <abdullah@daarb.dev>
2024-03-07 07:48:08 +0300

    removed non dbg commands from parser

--- command_parser.go
+++ command_parser.go
@@ -11,33 +11,13 @@ type CommandInput struct {
 }
 
 var commands = map[string]func(CommandInput) string{
-	"take": func(data CommandInput) string {
-		if len(data.inputs) < 2 {
-			return "I can't take nothing. Dumbass"
-		}
-
-		if data.inputs[1] == "key" {
-			data.g.hasKey = true
-			return "Taken"
-		}
-
-		return "something borked"
-	},
 	"quit": func(data CommandInput) string {
 		os.Exit(0)
 		return "exited"
 	},
-	"open": func(data CommandInput) string {
-		g := data.g
-		if len(data.inputs) < 2 {
-			return "I can't open nothing"
-		}
-
-		if g.hasKey {
-			return "You open the door and escape."
-		} else {
-			return "The door is locked"
-		}
+	"clear": func(data CommandInput) string {
+		data.g.Output = ""
+		return ""
 	},
 }
 
